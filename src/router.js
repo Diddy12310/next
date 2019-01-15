98ae639b9b4d8a3f516048dc214d8ba26e79bca2
Aidan Liddy <aidanliddy@outlook.com>
2019-01-15 16:37:15 -0500

    Changed some assets

--- src/router.js
+++ src/router.js
@@ -6,6 +6,12 @@ import Changelog from './views/Changelog'
 import Terms from './views/Terms'
 import Notice from './views/Notice'
 import Bookshelf from './views/Bookshelf'
+import DevNet from './views/DevNet'
+import Drawer from './views/Drawer'
+import Hex from './views/Hex'
+import Launchpad from './views/Launchpad'
+import Scorecard from './views/Scorecard'
+import Support from './views/Support'
 
 Vue.use(Router)
 
@@ -15,9 +21,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
-    },
+			redirect: '/home'
+		},
+		{
+			path: '/home',
+			name: 'Home',
+			component: Home
+		},
     {
       path: '/flamechat',
       name: 'Flamechat',
@@ -42,6 +52,36 @@ export default new Router({
 			path: '/bookshelf',
 			name: 'Bookshelf',
 			component: Bookshelf
+		},
+		{
+			path: '/devnet',
+			name: 'DevNet',
+			component: DevNet
+		},
+		{
+			path: '/drawer',
+			name: 'Drawer',
+			component: Drawer
+		},
+		{
+			path: '/hex',
+			name: 'Hex',
+			component: Hex
+		},
+		{
+			path: '/launchpad',
+			name: 'Launchpad',
+			component: Launchpad
+		},
+		{
+			path: '/scorecard',
+			name: 'Scorecard',
+			component: Scorecard
+		},
+		{
+			path: '/support',
+			name: 'Support',
+			component: Support
 		}
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="home">
-    <div style="text-align: center; height: calc(100% - 20px); background: url(./../assets/bread_back.jpg); background-attachment: fixed;" class="page-content">
+	<div class="home">
+  	<div style="text-align: center; height: calc(100% - 20px); background: url(./../assets/home_bg.jpg); background-attachment: fixed;" class="home">
       <h2 class="mdl-layout--large-screen-only" style="line-height: 70px; position: relative; text-align: center; margin: auto; padding-top: 15%; color: white;">A social media platform<br>built for the 21st century</h2>
       <h3 class="mdl-layout--small-screen-only" style="line-height: 50px; position: relative; text-align: center; margin: auto; padding-top: 30%; color: white;">A social media platform<br>built for the 21st century</h3>
       <router-link style="margin-top: 20px;" to="flamechat" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Open Flamechat</router-link>
-    </div>
-  </div>
+  	</div>
+		<h1>Announcements</h1>
+		<div v-for="(announcement, index) in announcements" :key="index" class="announcements mdl-card">
+			<div class="mdl-card__title">
+				<h3 class="mdl-card__title-text">{{ announcement.title }}</h3>
+			</div>
+			<div class="mdl-card__supporting-text"></div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -15,8 +22,21 @@ export default {
   name: 'home',
   data() {
     return {
-
-    }
+			announcements: []
+		}
+  },
+  created() {
+    db.collection('announcements').get().then(snapshot => {
+      snapshot.forEach(doc => {
+        let announcement = doc.data()
+        announcement.id = doc.id
+        this.announcements.push(announcement)
+      })
+    })
   }
 }
 </script>
+
+<style scoped>
+
+</style>
