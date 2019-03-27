f8bebbc1621b1102cab4a5c44c63ce51d8c6768f
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2019-03-27 07:04:16 -0400

    News UI changes

--- src/views/News.vue
+++ src/views/News.vue
@@ -6,7 +6,7 @@
 			<v-text-field v-model="searchNews" label="Search..." style="width: 300px; margin: 50px auto 0px auto;" hint="For now, you cannot search within articles."></v-text-field>
 		</div>
 
-		<v-card v-for="item in filteredNews" :key="item.timestamp">
+		<v-card v-for="item in filteredNews" :key="item.id" class="news-home">
 			<div v-if="item.uploaded">
 				<v-img :src="item.cover"></v-img>
 
@@ -16,12 +16,34 @@
 						<h4 class="subheading grey--text">{{ item.author }}&nbsp;&nbsp;|&nbsp;&nbsp;{{ item.timestamp }}</h4>
 					</div>
 				</v-card-title>
-				<v-divider></v-divider>
+				<!-- <v-divider></v-divider>
 				<v-card-text>
 					<div class="detitem" v-html="detitem" v-for="(detitem, index) in item.detail" :key="index"></div>
-				</v-card-text>
+				</v-card-text> -->
+				<v-card-actions>
+					<v-btn flat color="accent" @click="setNews(item.title, item.author, item.timestamp, item.cover, item.detail)">View Article</v-btn>
+				</v-card-actions>
 			</div>
     </v-card>
+
+
+		<v-dialog v-model="newsDialog" max-width="800" v-if="currentNews">
+			<v-card>
+				<v-img :src="currentNews.cover"></v-img>
+				<v-card-title primary-title>
+					<div>
+						<h3 class="headline mb-0">{{ currentNews.title }}</h3>
+						<h4 class="subheading grey--text">{{ currentNews.author }}&nbsp;&nbsp;|&nbsp;&nbsp;{{ currentNews.timestamp }}</h4>
+					</div>
+				</v-card-title>
+				<v-card-text>
+					<div class="detitem" v-html="detitem" v-for="(detitem, index) in currentNews.details" :key="index"></div>
+				</v-card-text>
+				<v-card-actions>
+					<v-btn flat color="accent" @click="newsDialog = false">Close</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
 
 	</div>
 </template>
@@ -36,12 +58,14 @@ export default {
 		return {
 			news: [],
 			searchNews: '',
-			username: ''
+			username: '',
+			currentNews: null,
+			newsDialog: false
 		}
 	},
 	created() {
 		this.username = this.$parent.$parent.$parent.username
-		let ref = db.collection('news').orderBy("stamp", "desc")
+		let ref = db.collection('news').orderBy("stamp", "desc").limit(25)
 
 		ref.onSnapshot(snapshot => {
 			snapshot.docChanges().forEach(change => {
@@ -82,6 +106,18 @@ export default {
 				return item.title.match(this.searchNews) || item.author.match(this.searchNews) || item.timestamp.match(this.searchNews)
 			})
 		}
+	},
+	methods: {
+		setNews(title, author, timestamp, cover, details) {
+			this.currentNews = {
+				title: title,
+				author: author,
+				timestamp: timestamp,
+				cover: cover,
+				details: details
+			}
+			this.newsDialog = true
+		}
 	}
 }
 </script>
@@ -95,7 +131,7 @@ export default {
 	padding-bottom: 0px;
 }
 
-div.v-card {
+div.v-card.news-home {
 	max-width: 750px;
 	width: 100%;
 	margin: 20px auto;
