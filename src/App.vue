ac5702b5ee79cc5a5c5a939b28d61bdb3be4919e
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2019-03-31 17:42:13 -0400

    Another bug fix

--- src/App.vue
+++ src/App.vue
@@ -570,12 +570,12 @@ export default {
 <style>
 html {
 	overflow-y: auto;
-	  -webkit-touch-callout: none;
-    	-webkit-user-select: none;
-    	 -khtml-user-select: none;
-      	 -moz-user-select: none;
-      	  -ms-user-select: none;
-      	      user-select: none;
+	-webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+	-moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 
 .clock {
