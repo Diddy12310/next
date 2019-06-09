65c871eb7e5495d8da20ed38ade4c7cca21c62d5
Aidan Liddy <aidanliddy@outlook.com>
2019-06-09 18:51:34 -0400

    Temporarily disabled notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,14 +1,14 @@
-importScripts('https://www.gstatic.com/firebasejs/6.1.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/6.1.0/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/6.1.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/6.1.0/firebase-messaging.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCQqPjLsa4fKf82dQ6V1iMxFlKgmDurnBA",
-  authDomain: "paradigm-a1bc9.firebaseapp.com",
-  databaseURL: "https://paradigm-a1bc9.firebaseio.com",
-  projectId: "paradigm-a1bc9",
-  storageBucket: "paradigm-a1bc9.appspot.com",
-  messagingSenderId: "728943503114",
-  appId: "1:728943503114:web:4ea6a4f7b7f57e71"
-});
+// firebase.initializeApp({
+//   apiKey: "AIzaSyCQqPjLsa4fKf82dQ6V1iMxFlKgmDurnBA",
+//   authDomain: "paradigm-a1bc9.firebaseapp.com",
+//   databaseURL: "https://paradigm-a1bc9.firebaseio.com",
+//   projectId: "paradigm-a1bc9",
+//   storageBucket: "paradigm-a1bc9.appspot.com",
+//   messagingSenderId: "728943503114",
+//   appId: "1:728943503114:web:4ea6a4f7b7f57e71"
+// });
 
-const msg = firebase.messaging();
+// const msg = firebase.messaging();
