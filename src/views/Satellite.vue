c58676117068aa99e011a1c5d99c6df7be3bc41e
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-04-15 17:44:59 -0400

    Quick links in Satellite

--- src/views/Satellite.vue
+++ src/views/Satellite.vue
@@ -3,8 +3,27 @@
 		<v-toolbar dense color="blue-grey darken-2">
 			<v-toolbar-title>Satellite</v-toolbar-title>
       <v-spacer></v-spacer>
-			<v-text-field color="white" class="mt-7" label="Web Address" v-model="input"></v-text-field>
-			<v-btn text icon @click="changeURL()"><v-icon>mdi-arrow-right</v-icon></v-btn>
+			<v-text-field color="white" class="mt-7" label="Address..." v-model="input"></v-text-field>
+			<v-menu offset-y v-model="quick_links">
+				<template v-slot:activator="{ on: menu }">
+					<v-tooltip bottom>
+						<template v-slot:activator="{ on: tooltip }">
+							<v-btn text icon v-on="{ ...tooltip, ...menu }"><v-icon>{{ quick_links ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon></v-btn>
+						</template>
+						<span>Quick Links</span>
+					</v-tooltip>
+				</template>
+				<v-list dense>
+					<v-list-item :input-value="input == 'https://www.bing.com/'" @click="input = 'https://www.bing.com/'">
+						<v-list-item-icon><v-icon>mdi-magnify</v-icon></v-list-item-icon>
+						<v-list-item-title>Search</v-list-item-title>
+					</v-list-item>
+					<v-list-item :input-value="input == 'https://www.theepochtimes.com/'" @click="input = 'https://www.theepochtimes.com/'">
+						<v-list-item-icon><v-icon>mdi-newspaper</v-icon></v-list-item-icon>
+						<v-list-item-title>News</v-list-item-title>
+					</v-list-item>
+				</v-list>
+			</v-menu>
     </v-toolbar>
 
 		<iframe id="satellite-embed" crossorigin="anonymous" :src="input"></iframe>
@@ -18,7 +37,8 @@ export default {
 		return {
 			proxy: 'https://cors.io/?',
 			input: 'https://www.bing.com/',
-			output: 'https://cors.io/?https://www.bing.com/'
+			output: 'https://cors.io/?https://www.bing.com/',
+			quick_links: false
 		}
 	},
 	methods: {
