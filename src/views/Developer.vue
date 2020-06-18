1187968f0addd99c461ce005a0d503c650d7f3dc
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-06-18 07:28:52 -0400

    Created Developer page

---
+++ src/views/Developer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="developer">
+    <v-toolbar dense color="blue-grey darken-4">
+      <v-toolbar-title>Developer</v-toolbar-title>
+    </v-toolbar>
+    <v-tabs v-model="tab" centered background-color="blue-grey darken-4" color="white">
+      <v-tab>Dashboard</v-tab>
+      <v-tab>Timeline</v-tab>
+      <v-tab>Documentation</v-tab>
+      <v-tab>Resources</v-tab>
+    </v-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Developer',
+  data() {
+    return {
+      tab: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
