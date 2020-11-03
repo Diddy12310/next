3d6984c458d482cf76bfad167dc13a18346309e4
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-11-03 05:56:55 -0500

    Began work on Preflight Engine

---
+++ src/components/PreflightToolbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app-bar app color="#0F1E3C">
+    <v-slide-x-transition group>
+      <div class="d-flex align-center" key="logo" v-if="!$root.notification">
+        <v-img
+          contain
+          src="../assets/logo.png"
+          width="40"
+          style="border-radius: 9999px"
+          class="ma-2"
+        />
+      </div>
+
+      <div class="d-flex" v-else key="notification">
+        <v-icon :class="{ [`${$root.notification.class}`]: true }">{{
+          $root.notification.icon
+        }}</v-icon>
+        <p
+          class="mb-0 pb-0 ml-2"
+          :class="{ [`${$root.notification.class}`]: true }"
+        >
+          {{ $root.notification.text }}
+        </p>
+      </div>
+    </v-slide-x-transition>
+
+    <v-spacer></v-spacer>
+
+    <p
+      class="font-weight-light text-h6 mb-0"
+      :class="{
+        centralize: $vuetify.breakpoint.smAndUp,
+        'text-right': $vuetify.breakpoint.xsOnly,
+      }"
+    >
+      <span v-if="!$root.user.password.includes('$2a$')"
+        >Complete Account Creation</span
+      >
+      <span v-if="$root.user.preflight.in_recovery">Account Recovery</span>
+      <span
+        v-if="$root.user.preflight.violation"
+        class="font-weight-black red--text"
+        >NOTICE OF VIOLATION</span
+      >
+    </p>
+
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "PreflightToolbar",
+};
+</script>
+
+<style></style>
---
+++ src/pages/preflight/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :style="{ height: `calc(100vh - 64px)`, overflowY: 'auto' }">
+    <Violation v-if="$root.user.preflight.violation" />
+  </div>
+</template>
+
+<script>
+import Violation from "./Violation.vue";
+
+export default {
+  name: "Preflight",
+  components: {
+    Violation,
+  },
+};
+</script>
+
+<style scoped>
+</style>
