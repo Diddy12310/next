f84e512d411db10df219005f1036d0cdb1cba4a7
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-11-14 09:03:13 -0500

    Programmatic home page article

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,20 @@
 <template>
   <div style="height: calc(100vh - 64px); overflow-y: auto" class="bg-img">
     <div style="padding: 15vh 2rem 2rem 2rem; max-width: 72rem" class="mx-auto">
-      <h6 class="grey--text text--darken-1 overline">October 1st, 2020</h6>
+      <h6 class="grey--text text--darken-1 overline">
+        {{ $root.config.landing.date }}
+      </h6>
       <h1
         style="max-width: 56rem"
         class="deep-purple--text text--darken-3 font-weight-bold text-h3"
-      >
-        Introducing a brand new interface!
-      </h1>
+        v-html="$root.config.landing.title"
+      ></h1>
       <p
         style="max-width: 36rem"
         class="grey--text text--lighten-1 text-h6 mt-3 mb-12 font-weight-light"
-      >
-        Our old interface had gotten old and rotten. So we got to work on a
-        complete rewrite with a fresh interface. As you can see, our beautiful
-        new interface has been publicly released.
-      </p>
-      <button class="d-flex items-center">
+        v-html="$root.config.landing.body"
+      ></p>
+      <!-- <button class="d-flex items-center">
         <div
           @click="$noFunc()"
           style="border-radius: 9999px"
@@ -26,7 +24,7 @@
           More articles
           <v-icon>mdi-chevron-right</v-icon>
         </div>
-      </button>
+      </button> -->
     </div>
 
     <button
