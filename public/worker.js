0486e366bd13529fe444cc718945f3296b43753c
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-11-15 11:14:36 -0500

    PWA improvements

--- public/worker.js
+++ public/worker.js
@@ -1,3 +1,13 @@
+const cacheName = "v1";
+
+const cacheAssets = [
+  "index.html",
+  // "/css/*.*",
+  // "/js/*.*",
+  // "/img/*.*",
+  "favicon.png"
+];
+
 console.log("Service Worker Loaded...");
 
 self.addEventListener("push", e => {
@@ -7,4 +17,42 @@ self.addEventListener("push", e => {
     body: data.body,
     icon: "https://www.theparadigmdev.com/relay/img/paradigm.png"
   });
+});
+
+// Call Install Event
+self.addEventListener("install", e => {
+  console.log("Service Worker: Installed");
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log("Service Worker: Caching Files");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Call Activate Event
+self.addEventListener("activate", e => {
+  console.log("Service Worker: Activated");
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log("Service Worker: Clearing Old Cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  console.log("Service Worker: Fetching");
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
